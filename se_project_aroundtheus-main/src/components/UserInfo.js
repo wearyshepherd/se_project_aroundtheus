c09f06ce3954363ed2bbbf31ee53c08fd614357b
Carlos Chavez <133473576+wearyshepherd@users.noreply.github.com>
2023-09-25 22:24:14 -0700

    Add files via upload

---
+++ se_project_aroundtheus-main/src/components/UserInfo.js
@@ -0,0 +1,25 @@
+export default class UserInfo {
+  constructor(nameSelector, jobSelector, avatarSelector) {
+    this._nameElem = document.querySelector(nameSelector);
+    this._jobElem = document.querySelector(jobSelector);
+    this._avatarElem = document.querySelector(avatarSelector);
+  }
+
+  getUserInfo() {
+    return {
+      name: this._nameElem.textContent,
+      job: this._jobElem.textContent,
+      avatar: this._avatarElem.src,
+    };
+  }
+
+  setUserInfo({ name, job }) {
+    this._nameElem.textContent = name;
+    this._jobElem.textContent = job;
+  }
+
+  setAvatar(avatar) {
+    this._avatarElem.src = avatar;
+    this._avatarElem.alt = this._nameElem.textContent;
+  }
+}
